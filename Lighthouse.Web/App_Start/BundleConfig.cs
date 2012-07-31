9e246f9c0e8bffab0d9838344e17068a3750ce7b
Bates Westmoreland <rbwestmoreland@gmail.com>
2012-07-30 22:38:48 -0400

    Added Twitter Bootstrap
    Added Twitter Bootstrap NuGet package

--- Lighthouse.Web/App_Start/BundleConfig.cs
+++ Lighthouse.Web/App_Start/BundleConfig.cs
@@ -8,9 +8,14 @@ namespace Lighthouse.Web
         public static void RegisterBundles(BundleCollection bundles)
         {
             var cssBundle = new Bundle("~/content/css", new CssMinify());
+            cssBundle.Include("~/content/bootstrap.min.css");
+            cssBundle.Include("~/content/bootstrap-custom.css");
+            cssBundle.Include("~/content/bootstrap-responsive.min.css");
+            cssBundle.Include("~/content/lighthouse.base.css");
             bundles.Add(cssBundle);
 
             var javascriptBundle = new Bundle("~/scripts/javascript", new JsMinify());
+            javascriptBundle.Include("~/scripts/bootstrap.min.js");
             bundles.Add(javascriptBundle);
 
             BundleTable.EnableOptimizations = true;
