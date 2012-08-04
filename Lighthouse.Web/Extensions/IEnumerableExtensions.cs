7436f101e7a7da0385578fd11f24ce19afd0f747
Bates Westmoreland <rbwestmoreland@gmail.com>
2012-08-04 12:20:28 -0400

    Added IEnumerableExtensions Class
    Added IEnumerableExtensions class with IsNullOrEmpty and
    IsNotNullOrEmpty members

---
+++ Lighthouse.Web/Extensions/IEnumerableExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace System.Collections.Generic
+{
+    public static class IEnumerableExtensions
+    {
+        public static bool IsNotNullOrEmpty<T>(this IEnumerable<T> enumerable)
+        {
+            return enumerable != null || enumerable.Any();
+        }
+
+        public static bool IsNullOrEmpty<T>(this IEnumerable<T> enumerable)
+        {
+            return enumerable == null || !enumerable.Any();
+        }
+    }
+}
