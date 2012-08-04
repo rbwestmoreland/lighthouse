acce8104e87fb9d52ceda869651de392ee39d5c1
Bates Westmoreland <rbwestmoreland@gmail.com>
2012-08-03 23:02:22 -0400

    Added Application Members
    Added GetCurrentStatus member
    Added AverageBuildTime member

--- Lighthouse.Web/Models/Dashboard/Application.cs
+++ Lighthouse.Web/Models/Dashboard/Application.cs
@@ -11,5 +11,35 @@ namespace Lighthouse.Web.Models.Dashboard
         public string Slug { get; set; }
         public string Url { get; set; }
         public IEnumerable<Build> Builds { get; set; }
+
+        public string GetCurrentStatus()
+        {
+            if (Builds != null && Builds.Any())
+            {
+                return Builds.First().Status.ToLower();
+            }
+            else
+            {
+                return "no builds";
+            }
+        }
+
+        public TimeSpan AverageBuildTime()
+        {
+            var averageBuildTime = TimeSpan.Zero;
+
+            if (Builds != null && Builds.Any())
+            {
+                var completedBuilds = Builds.Where(b => b.Created.HasValue && b.Deployed.HasValue);
+                if (completedBuilds.Any())
+                {
+                    var averageBuildSeconds = completedBuilds.Average(b => (b.Deployed.Value - b.Created.Value).TotalSeconds);
+                    var roundedUpAverageBuildSeconds = (int)Math.Round(averageBuildSeconds, MidpointRounding.AwayFromZero);
+                    averageBuildTime = new TimeSpan(0, 0, roundedUpAverageBuildSeconds);
+                }
+            }
+
+            return averageBuildTime;
+        }
     }
 }
