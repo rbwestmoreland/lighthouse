3e146664cc956aa43bdfb3ba8dc9e0d784edd0f4
Bates Westmoreland <rbwestmoreland@gmail.com>
2012-08-04 00:11:37 -0400

    Added StringExtensions Class
    Added StringExtensions class with SafeSubString members

---
+++ Lighthouse.Web/Extensions/StringExtensions.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace System
+{
+    public static class StringExtensions
+    {
+        public static string SafeSubString(this string text, int startIndex)
+        {
+            if (text.Length >= startIndex)
+            {
+                return text.Substring(startIndex);
+            }
+            else
+            {
+                return text;
+            }
+        }
+
+        public static string SafeSubString(this string text, int startIndex, int length)
+        {
+            if (text.Length >= startIndex + length)
+            {
+                return text.Substring(startIndex, length);
+            }
+            else
+            {
+                return text;
+            }
+        }
+    }
+}
