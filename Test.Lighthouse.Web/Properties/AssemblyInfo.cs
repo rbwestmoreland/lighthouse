4c1a3a0c9febe837efea34bb2d150f7d9c5143cf
Bates Westmoreland <rbwestmoreland@gmail.com>
2012-08-05 12:39:53 -0400

    Added Test.Lighthouse.Web Project
    Added an empty Test.Lighthouse.Web project with Lighthouse.Web and xUnit
    references

---
+++ Test.Lighthouse.Web/Properties/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Test.Lighthouse.Web")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: Guid("1950aaea-f858-492c-b837-42babfe8d65c")]
